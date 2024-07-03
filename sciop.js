97ee2803e1b6d37a490b478f05b5266df42d5123
lai <a0908370791@gmail.com>
2024-07-03 12:05:49 +0800

    nit

---
+++ sciop.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById('test');
+
+btn.addEventListener("click", () => {　 
+  alert("點到我了");                     
+  btn.disabled = true;                  
+});
+
+
+
+
+
+
+
+
